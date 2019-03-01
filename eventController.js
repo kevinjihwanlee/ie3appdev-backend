849f3a706d32b50f5e8389e7791e749c4d4c5c4e
Kevin J Lee <kevin.jihwan.lee@gmail.com>
2019-03-01 14:13:03 -0600

    fixed update function

--- eventController.js
+++ eventController.js
@@ -74,43 +74,52 @@ exports.view = function (req, res) {
 
 // Handle update event info
 exports.update = function (req, res) {
-  Event.findById(req.params.event_id, function (err, contact) {
+  Event.findById(req.params.event_id, function (err, event) {
     if (err) {
       res.json({
         status: 'error',
         message: err,
       })
     }
-  })
-  event.name = req.body.name
-  event.author = req.body.author 
-  event.coordinate.latitude = req.body.coordinate.latitude
-  event.coordinate.longitude = req.body.coordinate.longitude
-  // optional values
-  if ("description" in req.body) {
-    event.description = req.body.description
-  }
-  if ("start_time" in req.body) {
-    event.start_time = req.body.start_time
-  }
-  if ("end_time" in req.body) {
-    event.end_time = req.body.end_time
-  }
-  if("location" in req.body) {
-    event.location = req.body.location
-  }
-
-  // save event and check for errors
-  event.save(function (err) {
-    if (err) {
-      res.json({
-        status: 'error',
-        message: err,
-      })
+    if ("name" in req.body) {
+      event.name = req.body.name 
     }
-    res.json({
-      message: 'New event created',
-      data: event
+    if ("author" in req.body) {
+      event.author = req.body.author 
+    }
+    if ("coordinate" in req.body) {
+      // a little lazy by not enforcing both lat & long
+      event.coordinate.latitude = req.body.coordinate.latitude
+      event.coordinate.longitude = req.body.coordinate.longitude
+    }
+    if ("description" in req.body) {
+      event.description = req.body.description
+    }
+    if ("start_time" in req.body) {
+      event.start_time = req.body.start_time
+    }
+    if ("end_time" in req.body) {
+      event.end_time = req.body.end_time
+    }
+    if ("location" in req.body) {
+      event.location = req.body.location
+    }
+    if ("saved" in req.body) {
+      event.saved = req.body.saved
+    }
+  
+    // save event and check for errors
+    event.save(function (err) {
+      if (err) {
+        res.json({
+          status: 'error',
+          message: err,
+        })
+      }
+      res.json({
+        message: 'New event created',
+        data: event
+      })
     })
   })
 }
